c2586c9f66c11de64954728146f3755bd3a1bd46
Toby Jackson <tobyjackson@paypoint.net>
2014-08-11 14:48:58 +0100

    Fixing the sinatra app

--- callback.rb
+++ callback.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
 post '/' do
-  puts URI.unescape( params[:data] )
+  URI.unescape( params[:data] )
 end
 
 get '/' do
-  puts "Only POST is supported"
+  "Only POST is supported"
 end
