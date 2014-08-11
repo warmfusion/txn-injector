8a70b43891f056c2fb289609a4ea68496c0c1c60
Toby Jackson <tobyjackson@paypoint.net>
2014-08-11 15:20:20 +0100

    Added a few new callback methods - GET /callback returns json of incoming requests and GET /callback/clear resets the array

--- callback.rb
+++ callback.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
+require 'json'
+
+requests = []
 
 post '/callback' do
   URI.unescape( params[:data] )
+  requests << request
 end
 
 get '/' do
   "Please post your query to /callback?data=someValue "
+end
+
+get '/callback' do
+  requests.to_json()
+end
+
+
+get '/callback/clear' do
+  requests = []
+  "Request array cleared"
 end
