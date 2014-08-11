52f86f3846515ee6771b196774fa8863129b76a8
Toby Jackson <tobyjackson@paypoint.net>
2014-08-11 15:31:27 +0100

    Added a GET request on callbacks that actually works

--- callback.rb
+++ callback.rb
@@ -4,8 +4,12 @@ require 'json'
 requests = []
 
 post '/callback' do
+  request.body.rewind
+  requests << { "body"  => request.body.read, 
+                "path"  => request.path_info,
+                "query" => request.query_string
+              }
   URI.unescape( params[:data] )
-  requests << request
 end
 
 get '/' do
