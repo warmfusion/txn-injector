0700a7e36d352c998ab7943bac9cdf01315bc924
Toby Jackson <tobyjackson@paypoint.net>
2014-08-11 14:43:23 +0100

    initial test callback handler

---
+++ callback.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+
+post '/' do
+  puts URI.unescape(params[:data])
+end
+
+get '/' do
+  puts "Only POST is supported"
+end
---
+++ config.ru
@@ -0,0 +1,1 @@
+require './callback'run Sinatra::Application
