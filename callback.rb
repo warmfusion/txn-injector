6bc2ed7bd594fd32c4d9004226fea3414ddbd204
Toby Jackson <tobyjackson@paypoint.net>
2014-08-12 14:27:44 +0100

    Removed the notify handler as its basically the same as the callback ones and i'll probably just filter based on some other property in the incoming url

--- callback.rb
+++ callback.rb
@@ -2,39 +2,40 @@ require 'sinatra'
 require 'json'
 
 MAX_REQUEST_ARRAY_SIZE=500
-requests = {}
-requests[:callback] = []
-requests[:notify]   = []
+requests = []
 
 get '/' do
   content_type :json  
   response = { "introduction" => "Tobys Trivial Txn Injector",
-               "operations" => { "callback" => "%scallback" % request.url,
-                                 "notify" => "%snotify" % request.url },
+               "operations" => { "callback" => "%scallback" % request.url },
              }.to_json()
 end
 
 
 # CALLBACK HANDLERS
 post '/callback' do
-  request.body.rewind
-  requests[:callback] << { 
-                "body"  => request.body.read, 
-                "path"  => request.path_info,
-                "query" => request.query_string,
-                "date"  => DateTime.now()
-              }
-
-  # Pop the top off the requets lists if it gets too large :-)
-  while requests[:callback].length > MAX_REQUEST_ARRAY_SIZE do
-     requests[:callback].shift
-  end 
-     
   unless params[:data].nil?
      body URI.unescape( params[:data] )
   else
      body "OK"
   end
+
+
+  request.body.rewind
+  requests << { 
+		"id"       => params[:id],
+                "date"     => DateTime.now(),
+                "path"     => request.path_info,
+                "query"    => request.query_string,
+                "body"     => request.body.read, 
+                "response" => body
+              }
+
+  # Pop the top off the requets lists if it gets too large :-)
+  while requests.length > MAX_REQUEST_ARRAY_SIZE do
+     requests.shift
+  end 
+     
 end
 
 get '/callback' do
@@ -49,7 +50,7 @@ get '/callback' do
                                  "page_count" => ((requests.length / size).floor() +1), 
                                  "total_requests" => requests.length ,
                                 },
-               "requests" => requests[:callback].reverse.slice((from -1) * size ,size)
+               "requests" => requests.reverse.slice((from -1) * size ,size)
              }.to_json()
 end
 
@@ -59,46 +60,4 @@ get '/callback/clear' do
   { :request => "cleared" }.to_json()
 end
 
-
-#
-
-# NOTIFY HANDLERS
-post '/notify' do
-  request.body.rewind
-  requests[:notify] << { 
-                "body"  => request.body.read, 
-                "path"  => request.path_info,
-                "query" => request.query_string,
-                "date"  => DateTime.now()
-              }
-
-  # Pop the top off the requets lists if it gets too large :-)
-  while requests[:notify].length > MAX_REQUEST_ARRAY_SIZE do
-     requests[:notify].shift
-  end 
-     
-  body "OK"
-end
-
-get '/notify' do
-  content_type :json
-  size=10
-  from = params[:page].nil? ? 1 : params[:page].to_i
-
-  response = { 
-               "operations" => { "clear" => "%s/clear" % request.url },
-               "pagination" => { "page" => from, 
-                                 "page_size" => size ,
-                                 "page_count" => ((requests.length / size).floor() +1), 
-                                 "total_requests" => requests.length ,
-                                },
-               "requests" => requests[:notify].reverse.slice((from -1) * size ,size)
-             }.to_json()
-end
-
-get '/notify/clear' do
-  requests[:notify] = []
-  content_type :json
-  { :request => "cleared" }.to_json()
-end
 
